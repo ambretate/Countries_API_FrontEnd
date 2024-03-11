b1c0db48716c331f754d0d7a67d09c10f970cede
Ambre Tate <ambretate@gmail.com>
2024-03-11 12:04:32 -0400

    updated App.jsx with Routes

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Nav from "./components.Nav.jsx"
+import Home from "./screens/Home.jsx"
+import Countries from "./screens/Countries.jsx"
+import CountriesCreate from "./screens/CountriesCreate.jsx"
+import CountriesDetail from "./screens/CountriesDetail.jsx"
+import CountriesEdit from "./screens/CountriesEdit.jsx"
+import { Routes, Route } from "react-router-dom"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/countries" element={<Countries />} />
+        <Route path="/add-country" element={<CountriesCreate />} />
+        <Route path="/countries/:id" element={<CountriesDetail />} />
+        <Route path="/countries/:id/edit" element={<CountriesEdit />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
